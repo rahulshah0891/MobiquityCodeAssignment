c6be85dad24f2ab93c5a459ffee946e3c5618b1f
rahulshah0891 <rahulshah0891@gmail.com>
2021-04-17 20:23:26 +0530

    Initial commit

---
+++ app/src/main/java/com/mobiquityassignment/ui/base/BaseFragment.kt
@@ -0,0 +1,34 @@
+package com.mobiquityassignment.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseFragment<VB : ViewDataBinding, VM : ViewModel> : Fragment() {
+
+    protected lateinit var dataBinding: VB
+    protected lateinit var viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, setLayout(), container, false)
+        viewModel = ViewModelProvider(this).get(getViewModelClass())
+        return dataBinding.root
+    }
+
+    abstract fun setLayout(): Int
+
+    private fun getViewModelClass(): Class<VM> {
+        return (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<VM>
+    }
+}
